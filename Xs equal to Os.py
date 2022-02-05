64804ff608f990ae8d97cef4dc189f5cea1e9cc8
sergiomendozer <sergiomendozansx@gmail.com>
2022-02-04 22:09:12 -0800

    reformatted

--- Xs equal to Os.py
+++ Xs equal to Os.py
@@ -1,14 +1,15 @@
 def counts(string):
-    count_x=0
-    count_o=0
-    i=-1
-    while i<len(string)-1:
-        i+=1
-        if string[i]=="x":
-            count_x=count_x+1
-        if string[i]=="o":
-            count_o=count_o+1
-        
+    count_x = 0
+    count_o = 0
+    i = -1
+    while i < len(string) - 1:
+        i += 1
+        if string[i] == "x":
+            count_x = count_x + 1
+        if string[i] == "o":
+            count_o = count_o + 1
+
     return count_o == count_x
 
-print(counts ("xoxoi3xo"))
+
+print(counts("xoxoi3xo"))
